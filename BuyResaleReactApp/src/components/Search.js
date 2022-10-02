2571dce4e1318cc33d31abc201df535aee8accb6
Azheem Rahman <azheem.rahman@gmail.com>
2022-10-02 15:10:56 +0800

    import context into Search component

--- BuyResaleReactApp/src/components/Search.js
+++ BuyResaleReactApp/src/components/Search.js
@@ -1,7 +1,9 @@
 // Search component to take in user's input & selection choices
-import React from "react";
+import React, { useContext } from "react";
+import SomeContext from "../context/some-context";
 
 const Search = () => {
+  const someCtx = useContext(SomeContext);
   return <div></div>;
 };
 
