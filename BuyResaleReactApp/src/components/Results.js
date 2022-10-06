6e3c744c3cdff774fba3baa3e0acc5cb7eb6f9c8
Azheem Rahman <azheem.rahman@gmail.com>
2022-10-06 12:56:52 +0800

    updated Results.js to align results display

--- BuyResaleReactApp/src/components/Results.js
+++ BuyResaleReactApp/src/components/Results.js
@@ -12,97 +12,112 @@ const Results = () => {
       <div className="container">
         <div className="row">
           <div className="col">
-            <h1 className="centered">Results for {someCtx.searchCriteria}</h1>
+            <h1 className="centered">
+              Results for {someCtx.town}, {someCtx.flatType},{" "}
+              {someCtx.flatModel}
+            </h1>
           </div>
         </div>
         <div className="row">
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Street Name</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Block</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Storey Range</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Flat Type</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Flat Model</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Floor Area sqm</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Resale Price</strong>
             </label>
           </div>
-          <div className="col">
-            <label>
+          <div className="col d-flex justify-content-center">
+            <label className="text-justify-center">
               <strong>Remaining Lease</strong>
             </label>
           </div>
         </div>
         <br />
         <div className="row">
-          <div className="col">
-            {someCtx.post.map((item, index) => {
-              if (JSON.stringify(someCtx.town) === JSON.stringify(item.town)) {
+          {someCtx.post.map((item, index) => {
+            if (JSON.stringify(someCtx.town) === JSON.stringify(item.town)) {
+              if (
+                JSON.stringify(someCtx.flatType) ===
+                JSON.stringify(item.flat_type)
+              ) {
                 if (
-                  JSON.stringify(someCtx.flatType) ===
-                  JSON.stringify(item.flat_type)
+                  JSON.stringify(someCtx.flatModel) ===
+                  JSON.stringify(item.flat_model)
                 ) {
-                  if (
-                    JSON.stringify(someCtx.flatModel) ===
-                    JSON.stringify(item.flat_model)
-                  ) {
-                    return (
-                      <div className="row" key={index}>
-                        <div className="col">
-                          <label>{item.street_name}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.block}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.storey_range}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.flat_type}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.flat_model}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.floor_area_sqm}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.resale_price}</label>
-                        </div>
-                        <div className="col">
-                          <label>{item.remaining_lease}</label>
-                        </div>
+                  return (
+                    <div className="row" key={index}>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.street_name}
+                        </label>
                       </div>
-                    );
-                  }
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.block}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.storey_range}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.flat_type}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.flat_model}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.floor_area_sqm}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label className="text-justify-center">
+                          {item.resale_price}
+                        </label>
+                      </div>
+                      <div className="col d-flex justify-content-center">
+                        <label>{item.remaining_lease}</label>
+                      </div>
+                    </div>
+                  );
                 }
               }
-            })}
-          </div>
+            }
+          })}
         </div>
       </div>
 
